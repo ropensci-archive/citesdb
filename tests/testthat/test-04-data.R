4e3ee03fa0e6477a6c9b5e55719907018ce4b8bd
Noam Ross <noam.ross@gmail.com>
2019-03-18 10:25:56 -0400

    Use tmpfile for testing data download

--- tests/testthat/test-04-data.R
+++ tests/testthat/test-04-data.R
@@ -2,9 +2,9 @@ context("Data")
 
 test_that("CITES data source hasn't changed", {
   skip_on_cran()
+  tmpf <- tempfile()
   httr::GET("https://trade.cites.org/cites_trade/download_db",
-            httr::write_disk("cites_trade_db.zip"))
-  expect_equivalent(tools::md5sum("cites_trade_db.zip"),
-                    "3d774b109c3ebf3594cf0fd4c20c1d1b")
-  unlink("cites_trade_db.zip")
+            httr::write_disk(tmpf))
+  expect_equivalent(tools::md5sum(tmpf), "3d774b109c3ebf3594cf0fd4c20c1d1b")
+  unlink(tmpf)
 })
