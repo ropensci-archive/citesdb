a5429516bc78ff05e6937094e6573acda6ac719c
Noam Ross <ross@ecohealthalliance.org>
2019-05-08 10:04:22 -0400

    Exclude linters that trigger false positives.
    Closes #2

--- tests/testthat/test-lint.R
+++ tests/testthat/test-lint.R
@@ -2,6 +2,11 @@ if (requireNamespace("lintr", quietly = TRUE)) {
   context("lints")
   test_that("Package Style", {
     skip_on_cran()
-    lintr::expect_lint_free()
+    lintr::expect_lint_free(
+      linters = lintr::with_defaults(
+        camel_case_linter = NULL,
+        snake_case_linter = NULL,
+        absolute_paths_linter = NULL
+      ))
   })
 }
