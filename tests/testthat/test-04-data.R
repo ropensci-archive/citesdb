48aeb3dc40bb724383810ae7c6065d3245a85719
Noam Ross <ross@ecohealthalliance.org>
2019-05-15 11:45:26 -0400

    Make data update test robust to conneection errors

--- tests/testthat/test-04-data.R
+++ tests/testthat/test-04-data.R
@@ -3,8 +3,13 @@ context("Data")
 test_that("CITES data source hasn't changed", {
   skip_on_cran()
   tmpf <- tempfile()
-  httr::GET("https://trade.cites.org/cites_trade/download_db",
-            httr::write_disk(tmpf))
-  expect_equivalent(tools::md5sum(tmpf), "3d774b109c3ebf3594cf0fd4c20c1d1b")
+  response <- httr::GET("https://trade.cites.org/cites_trade/download_db",
+                        httr::write_disk(tmpf, overwrite = TRUE))
+  if (httr::http_error(response)) {
+    message("CITES data source not responding")
+  }
+  hash <- tools::md5sum(tmpf)
   unlink(tmpf)
+  skip_if(httr::http_error(response))
+  expect_equivalent(hash, "3d774b109c3ebf3594cf0fd4c20c1d1b")
 })
