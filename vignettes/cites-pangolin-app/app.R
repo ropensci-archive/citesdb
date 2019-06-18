f5d2ffc7cf5a9e9d99b9aab11759b8d1b0324ac7
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2019-06-18 15:14:14 -0400

    fix colors

--- vignettes/cites-pangolin-app/app.R
+++ vignettes/cites-pangolin-app/app.R
@@ -10,6 +10,7 @@ dat <- read_rds(here::here("vignettes/pangolin_dat.rds"))
 pal <- c("#FC97AF", "#87F7CF", "#F7F494", "#72CCFF", "#F7C5A0",
          "#D4A4EB", "#D2F5A6", "#76F2F2", "#D6D963", "#F4F4F0")
 names(pal) <- unique(as.character(dat$term))[sample(x = 10, 10)]
+pal <- pal[levels(dat$term)]
 
 ui <- fluidPage(
 
@@ -45,22 +46,25 @@ server <- function(input, output) {
         dat2 = dat %>%
             filter(year == input$year
             ) %>%
+            arrange(term) %>%
             group_by(term)
 
         sub_pal <- unname(pal[as.character(unique(dat2$term))])
 
-        dat2 %>%
-            e_charts(start_lon) %>%
+        e <- dat2 %>% e_charts(start_lon)
+        e$x$data <- e$x$data[sort(names(e$x$data ))]
+
+        e %>%
             e_geo(roam = TRUE,
                   label = list(emphasis = list(show = FALSE)),
                   # region = list(
                   #     name = "China",
                   #     itemStyle = list(areaColor = "red", color ='red')
-#https://ecomfe.github.io/echarts-doc/public/en/option.html#geo
-           itemStyle = list(normal = list(areaColor = '#323c48', borderColor = '#404a59'),
-                            emphasis = list(areaColor = '#323c48', borderColor = '#404a59')
-           )
-        ) %>%
+                  #https://ecomfe.github.io/echarts-doc/public/en/option.html#geo
+                  itemStyle = list(normal = list(areaColor = '#323c48', borderColor = '#404a59'),
+                                   emphasis = list(areaColor = '#323c48', borderColor = '#404a59')
+                  )
+            ) %>%
             e_lines(start_lon, start_lat, end_lon, end_lat, silent = FALSE,
                     source_name = start, target_name = end,
                     effect = list(show=T, period=3, trailLength=0, symbolSize=3),
