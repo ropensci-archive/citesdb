4d5e5f69be7da80366831032ec5beef800f71d0e
Noam Ross <ross@ecohealthalliance.org>
2019-11-22 17:18:09 -0500

    Removing lint test

--- tests/testthat/test-lint.R
+++ tests/testthat/test-lint.R
@@ -1,13 +1,13 @@
-if (requireNamespace("lintr", quietly = TRUE)) {
-  context("lints")
-  test_that("Package Style", {
-    skip_on_cran()
-    lintr::expect_lint_free(
-      linters = lintr::with_defaults(
-        camel_case_linter = NULL,
-        snake_case_linter = NULL,
-        absolute_paths_linter = NULL,
-        line_length_linter = lintr::line_length_linter(120)
-      ))
-  })
-}
+# if (requireNamespace("lintr", quietly = TRUE)) {
+#   context("lints")
+#   test_that("Package Style", {
+#     skip_on_cran()
+#     lintr::expect_lint_free(
+#       linters = lintr::with_defaults(
+#         camel_case_linter = NULL,
+#         snake_case_linter = NULL,
+#         absolute_paths_linter = NULL,
+#         line_length_linter = lintr::line_length_linter(120)
+#       ))
+#   })
+# }
